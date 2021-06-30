c20a285c25c458cbe68789ad01c30cc08c286ad0
Jack <huanghaoranhoo@gmail.com>
2021-06-30 16:00:24 +1000

    HTML, CSS, JaveScript review

---
+++ html review/JS/main.js
@@ -0,0 +1,25 @@
+// alert(1+1);
+
+// console.log("word".length);
+
+// console.log("word".charAt(0));
+
+// console.log("Hello word".replace("Hello","thank"));
+
+// console.log("Hello word".toUpperCase());
+
+var Myname = "Jack Huang";
+
+console.log(Myname);
+
+let a = 1;
+
+console.log(a);
+
+Myname = "Susie";
+
+console.log(Myname);
+
+const Pi = 3.14;
+
+console.log(Pi);
